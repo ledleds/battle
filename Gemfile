d69e0145999e33107b365d349438259813b84a5b
Vicky Ledsom <victorialedsom@gmail.com>
2017-03-02 12:45:22 +0000

    Add session to store name information

--- Gemfile
+++ Gemfile
@@ -3,3 +3,4 @@ source 'https://rubygems.org'
 gem "sinatra"
 gem 'rspec-sinatra', '~> 0.1.2'
 gem "capybara"
+gem 'rerun'
--- app.rb
+++ app.rb
@@ -1,6 +1,7 @@
 require 'sinatra/base'
 
 class BattleApp < Sinatra::Base
+  enable :sessions
 
   get '/' do
     "Welcome to Battle"
@@ -9,11 +10,16 @@ class BattleApp < Sinatra::Base
 
   post '/names' do
     p params
-    @player_1 = params[:player_1]
-    @player_2 = params[:player_2]
-    erb(:play)
+    session[:player_1] = params[:player_1]
+    session[:player_2] = params[:player_2]
+    redirect '/play'
   end
 
+  get '/play' do
+    @player_1 = session[:player_1]
+    @player_2 = session[:player_2]
+    erb(:play)
+  end
   # start the server if ruby file executed directly
   run! if app_file == $0
 end
