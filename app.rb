d30b03cf31116b96983fe02b37299997b7971e9b
Vicky Ledsom <victorialedsom@gmail.com>
2017-03-02 16:36:46 +0000

    Create attack button

--- app.rb
+++ app.rb
@@ -20,6 +20,12 @@ class BattleApp < Sinatra::Base
     @player_2 = session[:player_2_name]
     erb(:play)
   end
+
+  get '/attack' do
+    @player_1 = session[:player_1_name]
+    @player_2 = session[:player_2_name]
+    erb(:attack)
+  end
   # start the server if ruby file executed directly
   run! if app_file == $0
 end
--- spec/features/capybara_spec.rb
+++ spec/features/capybara_spec.rb
@@ -1,7 +1,6 @@
 require 'spec_helper'
 
 feature 'Player name input' do
-  # describe 'Entering player names', :type => :feature do
     scenario "should show player names after they input their name" do
       sign_in_and_play
       expect(page).to have_content("Link")
@@ -9,9 +8,17 @@ feature 'Player name input' do
     end
   end
 
-  feature 'View hitpoints of other users' do
-    scenario 'View players hitpoints' do
-      sign_in_and_play
-      expect(page).to have_content 'Link has 20HP'
-    end
+feature 'View hitpoints of other users' do
+  scenario 'View players hitpoints' do
+    sign_in_and_play
+    expect(page).to have_content 'Link has 20HP'
   end
+end
+
+feature 'Show confirmation after an attck' do
+  scenario 'Player 1 attacks Player 1' do
+    sign_in_and_play
+    click_button 'Attack!'
+    expect(page).to have_content('Link attacked Ganon')
+  end
+end
