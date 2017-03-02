74f7b8ed8e39943399640c7d4faf831c94e4dd45
John Chang <johnchang9094@gmail.com>
2017-03-02 14:48:07 +0000

    HP Added

--- spec/features/capybara_spec.rb
+++ spec/features/capybara_spec.rb
@@ -11,3 +11,13 @@ feature 'Player name input' do
       expect(page).to have_content("Ganon")
     end
   end
+
+  feature 'View hitpoints of other users' do
+    scenario 'View players hitpoints' do
+      visit "/"
+      fill_in :player_1, with: 'Link'
+      fill_in :player_2, with: 'Ganon'
+      click_button('Create Player')
+      expect(page).to have_content 'Link has 20HP'
+    end
+  end
